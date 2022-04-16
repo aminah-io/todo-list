e4e06dc5b9a2eebf9da2ec385d3165e09b7af7ac
Aminah Burch <hello@aminah.io>
2022-04-16 21:49:52 +0300

    Removed switch statements and added an array so the tasks can be added to them from the HTML file and added a post route

--- app.js
+++ app.js
@@ -10,46 +10,28 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+//----------- Global variables -----------
+let items = [];
 
 app.get('/', (req, res) => {
-  let today = new Date();
-  let theDay = today.getDay();
-  let actualDay;
-  
-  switch(theDay) {
-    case 0:
-      actualDay = 'Sunday';
-      break;
-    case 1:
-      actualDay = 'Monday';
-      break;
-    case 2:
-      actualDay = 'Tuesday';
-      break;
-    case 3:
-      actualDay = 'Wednesday';
-      break;
-    case 4:
-      actualDay = 'Thursday';
-      break;
-    case 5:
-      actualDay = 'Friday';
-      break;
-    case 6:
-      actualDay = 'Saturday';
-      break;
-    default:
-      console.log(`Error! The current day is equal to ${theDay}`);
+  let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
   }
   
-  res.render('list', { weekDay: actualDay });
-  // if (today.getDay() === 6 || today.getDay() === 0) {
-  //   res.send(`It's the weekend baby!`)
-  // } else if (today.getDay() === 5) {
-  //   res.send(`IT'S FRIDAY BABAY!`)
-  // } else {
-  //   res.send(`Welp, gotta party on a week day anyway BABY!`)
-  // }
+  let today = new Date;
+  let actualDate = today.toLocaleDateString('en-US', options);
+
+
+  res.render('list', { weekDay: actualDate, listItems: items });
+})
+
+app.post('/', (req, res) => {
+  let item = req.body.newItem
+  items.push(item);
+
+  res.redirect('/')
 })
 
 
